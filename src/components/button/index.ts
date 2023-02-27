eaa3d5d7746cc9263c97f5b708eca7cf8e84b520
Maria Anofrieva <mariyas12@yandex.ru>
2023-02-27 19:55:46 +0400

    fix: clear main branch

--- src/components/button/index.ts
+++
@@ -1,6 +0,0 @@
-import { registerPartial } from "../../utils/register-partial";
-
-import template from "./button.hbs";
-import * as style from "./style.module.scss";
-
-registerPartial({ name: "button", template, style });
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import "./components";
-import template from "./index.hbs";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const app = document.getElementById("root");
-
-  app.innerHTML = template({ text: "Test 123" });
-});
