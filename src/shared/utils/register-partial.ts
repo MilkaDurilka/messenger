da82510bcadd8c660bfa7eedaa174b18545698e4
Maria Anofrieva <mariyas12@yandex.ru>
2023-03-01 20:10:01 +0400

    feat: layout

--- src/shared/utils/register-partial.ts
+++ src/shared/utils/register-partial.ts
@@ -1,11 +1,7 @@
 import Handlebars from "handlebars/dist/handlebars.runtime";
 import { TRegisterPartial } from "./types";
 
-export const registerPartial = ({
-  name,
-  template,
-  style,
-}: TRegisterPartial): void => {
+export const registerPartial = ({ name, template, style }: TRegisterPartial): void => {
   Handlebars.registerPartial(name, (params, options) =>
     template.apply(null, [{ ...params, style }, options])
   );
